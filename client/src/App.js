855be666be4c723a1be897649aae27b487c988de
AYUSH <ayushrajbls@gmail.com>
2023-04-20 22:29:02 +0530

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,45 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="posts">
+        <div className="image"><img src="https://images.pexels.com/photos/16066100/pexels-photo-16066100.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" /></div>
+
+        <div className="texts">
+          <h2>Rainy day in seoul,korea</h2>
+          <p className="info">
+            <a href="" className="author">author</a>
+            <time>2023-01-01 15:40</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at hic et ad dicta aspernatur inventore officiis eum illo vel, accusamus autem iste cum voluptas facere iusto perferendis ducimus dolores fuga corporis in ipsa, deleniti maiores iure! Reiciendis corporis ad sint aut rerum, omnis modi, exercitationem eos numquam facilis maiores!</p>
+        </div>
+
+      </div>
+      <div className="posts">
+        <div className="image"><img src="https://images.pexels.com/photos/16066100/pexels-photo-16066100.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" /></div>
+
+        <div className="texts">
+          <h2>Rainy day in seoul,korea</h2>
+          <p className="info">
+            <a href="" className="author">author</a>
+            <time>2023-01-01 15:40</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at hic et ad dicta aspernatur inventore officiis eum illo vel, accusamus autem iste cum voluptas facere iusto perferendis ducimus dolores fuga corporis in ipsa, deleniti maiores iure! Reiciendis corporis ad sint aut rerum, omnis modi, exercitationem eos numquam facilis maiores!</p>
+        </div>
+
+      </div>
+      <div className="posts">
+        <div className="image"><img src="https://images.pexels.com/photos/16066100/pexels-photo-16066100.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="" /></div>
+
+        <div className="texts">
+          <h2>Rainy day in seoul,korea</h2>
+          <p className="info">
+            <a href="" className="author">author</a>
+            <time>2023-01-01 15:40</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi at hic et ad dicta aspernatur inventore officiis eum illo vel, accusamus autem iste cum voluptas facere iusto perferendis ducimus dolores fuga corporis in ipsa, deleniti maiores iure! Reiciendis corporis ad sint aut rerum, omnis modi, exercitationem eos numquam facilis maiores!</p>
+        </div>
+
+      </div>
     </main>
   );
 }
