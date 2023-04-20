12069f16cb892aefe4d9dd38936a6c003b2a74f3
AYUSH <ayushrajbls@gmail.com>
2023-04-20 23:33:18 +0530

    changes

--- client/src/Pages/RegisterPage.js
+++ client/src/Pages/RegisterPage.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
-  return (
-    <form className='register'>
-        <h1>Register</h1>
-    <input type="text" placeholder='username' />
-    <input type="password" placeholder='password'/>
-    <button>Register</button>
-  </form>
-  )
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const registerHandler =(e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <form className='register' onSubmit={registerHandler}>
+            <h1>Register</h1>
+            <input type="text" placeholder='username' value={username} onChange={(e) => setUserName(e.target.value)} />
+            <input type="password" placeholder='password' value={password}
+                onChange={(e) => setPassword(e.target.value)} />
+            <button>Register</button>
+        </form>
+    )
 }
 
 export default RegisterPage
