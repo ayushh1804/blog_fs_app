0fddcadfea92115fee41dd26340b629757021fdd
AYUSH <ayushrajbls@gmail.com>
2023-04-20 23:22:01 +0530

    login page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './Header';
 import Layout from './Layout';
 import Homepage from './Pages/Homepage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 import Post from './Post';
 import { Routes, Route } from "react-router-dom";
 function App() {
@@ -10,8 +12,8 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Homepage />} />
-        <Route path='/login' element={
-          <div>login page</div>} />
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/register' element={<RegisterPage />}/>
       </Route>
     </Routes>
 
