a3c7fb6c950eae97f7b548da79e0d5624185c075
AYUSH <ayushrajbls@gmail.com>
2023-04-20 22:56:04 +0530

    homepage added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 
 import './App.css';
 import Header from './Header';
+import Layout from './Layout';
+import Homepage from './Pages/Homepage';
 import Post from './Post';
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path='/' element={
-        <main>
-          <Header />
-          <Post />
-          <Post />
-          <Post />
-        </main>
-      } />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path='/login' element={
+          <div>login page</div>} />
+      </Route>
     </Routes>
 
   );
---
+++ client/src/Pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from '../Post'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default Homepage
