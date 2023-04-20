23f85c0da0034f2412ab516bbc759131c5a4160e
AYUSH <ayushrajbls@gmail.com>
2023-04-21 00:30:10 +0530

    register complete

--- api/index.js
+++ api/index.js
@@ -3,20 +3,31 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config()
+const User = require('./models/User')
 
 
-try{
+try {
     mongoose.connect(process.env.MONGO_CONNECTION_URL)
     console.log('Database Connection Successful')
-}catch(e){
-    console.log(e)
+} catch (e) {
+    alert(e)
 }
 
 app.use(cors())
 app.use(express.json())
-app.post('/register',(req,res) => {
-    const {username,password} = req.body;
-    res.json({requestdate:{username,password}})
+app.post('/register', async (req, res) => {
+  
+        const { username, password } = req.body;
+        const UserDocument =  await User.create({
+             username, password
+         })
+         res.json(UserDocument);
+ 
+      
+
+   
+
+    // res.json({ requestdate: { username, password } })
 
 })
 
