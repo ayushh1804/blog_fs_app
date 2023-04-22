bfab33033f2f4859f5fce5c44791b5f6a1d7107f
AYUSH <ayushrajbls@gmail.com>
2023-04-22 11:37:52 +0530

    react-quill added

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'react-router-dom'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link', 'image'],
+        ['clean']
+    ],
+}
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+];
 const CreatePost = () => {
-  return (
-    <form>
-        <input type="title" placeholder='Title' />
-        <input type="summary" placeholder='Summary' />
-        <input type="file" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
 
-    </form>
-  )
+    const [title, setTitle] = useState('')
+    const [summary, setSummary] = useState('')
+    const [content, setContent] = useState('')
+
+
+
+    return (
+        <form>
+            <input type="title" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} />
+            <input type="summary" placeholder='Summary' value={summary} onChange={e => setSummary(e.target.value)} />
+            <input type="file" />
+            <ReactQuill value={content} modules={modules} formats={formats} onChange={newValue => setContent(newValue)}/>
+            <button style={{ marginTop: '5px' }}>Create Blog</button>
+        </form>
+    )
 }
 
 export default CreatePost
