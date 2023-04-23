4058d702faf24b758aa60115db915accd26c0cc5
AYUSH <ayushrajbls@gmail.com>
2023-04-23 19:57:47 +0530

    changes

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,18 +1,21 @@
-import {format} from "date-fns";
-export default function Post({title,summary,cover,content,createdAt,author}) {
-    return(
-        <div className="posts">
-        <div className="image"><img src={'http://localhost:4000/'+ cover} alt="" /></div>
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+export default function Post({_id, title, summary, cover, content, createdAt, author }) {
+  return (
+    <div className="posts">
+      <Link to={`/post/${_id}`}> <div className="image"><img src={'http://localhost:4000/' + cover} alt="" /></div></Link>
 
-        <div className="texts">
-          <h2>{title}</h2>
-          <p className="info">
-            <a href="" className="author">{author.username}</a>
-            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
-          </p>
-          <p className='summary'>{summary}</p>
-        </div>
 
+      <div className="texts">
+        <Link to={`/post/${_id}`}>   <h2>{title}</h2></Link>
+
+        <p className="info">
+          <a href="" className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
+        </p>
+        <p className='summary'>{summary}</p>
       </div>
-    )
+
+    </div>
+  )
 }
