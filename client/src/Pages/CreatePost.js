0ad63b594db193f1769ecddca619e931c582fff7
AYUSH <ayushrajbls@gmail.com>
2023-04-23 18:52:40 +0530

    changes

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
@@ -23,27 +23,32 @@ const CreatePost = () => {
     const [title, setTitle] = useState('')
     const [summary, setSummary] = useState('')
     const [content, setContent] = useState('')
-    const [files,setFiles] = useState('')
-const data = new FormData();
-data.set('title',title);
-data.set('summary',summary);
-data.set('content',content);
-data.set('file',files[0])
-const createPostHandler = async (e) => {
-    e.preventDefault();
-    const response = await fetch('http://localhost:4000/post',{
-        method: 'POST',
-        body:data,
-    })
-    
+    const [files, setFiles] = useState('')
+    const [redirect, setRedirect] = useState(false);
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0])
+    const createPostHandler = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        })
+        if (response.status == 200) {
+            setRedirect(true)
+        }
+    }
+if(redirect){
+    return <Navigate to='/'/>
 }
-
     return (
         <form onSubmit={createPostHandler}>
             <input type="title" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)} />
             <input type="summary" placeholder='Summary' value={summary} onChange={e => setSummary(e.target.value)} />
             <input type="file" onChange={(e => setFiles(e.target.files))} />
-            <ReactQuill value={content} modules={modules} formats={formats} onChange={newValue => setContent(newValue)}/>
+            <ReactQuill value={content} modules={modules} formats={formats} onChange={newValue => setContent(newValue)} />
             <button style={{ marginTop: '5px' }}>Create Blog</button>
         </form>
     )
