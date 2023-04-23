d2d361d7c01e9f81e60bd4d69d627b476cf1460f
AYUSH <ayushrajbls@gmail.com>
2023-04-23 20:51:23 +0530

    changes

--- api/models/Post.js
+++ api/models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const PostSchema = new mongoose.Schema({
     title: String,
     summary: String,
-    Content: String,
+    content: String,
     cover: String,
     author:{type: mongoose.Schema.Types.ObjectId, ref:'User'}
 }, {
--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { formatISO9075 } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const PostPage = () => {
     const { id } = useParams();
@@ -16,9 +17,17 @@ const PostPage = () => {
     if (!postInfo) return '';
 
     return (
-        <div className='post-page'><div className='image'>
-            <img src={`http://localhost:4000/${postInfo.cover}`} alt='' />
-        </div></div>
+        <div className='post-page'>
+            <h1>{postInfo.title}</h1>
+            <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+            <div className="author">by @{postInfo.author.username}</div>
+            <div className='image'>
+                <img src={`http://localhost:4000/${postInfo.cover}`} alt='' />
+            </div>
+
+            {console.log(postInfo.content)}
+            <div className='content' dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+        </div>
 
     )
 }
