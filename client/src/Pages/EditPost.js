32eb94cedcec7337e24ebe7a7b6b92514ac4c5ee
AYUSH <ayushrajbls@gmail.com>
2023-04-24 00:08:06 +0530

    changes

--- client/src/Pages/EditPost.js
+++ client/src/Pages/EditPost.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { Navigate, useParams } from 'react-router-dom'
-import ReactQuill from 'react-quill'
+import React, { useEffect, useState } from 'react';
+import ReactQuill from 'react-quill';
+import { Navigate, useParams } from 'react-router-dom';
 
 const modules = {
     toolbar: [
@@ -19,43 +19,48 @@ const formats = [
 ];
 
 const EditPost = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const [title, setTitle] = useState('')
     const [summary, setSummary] = useState('')
     const [content, setContent] = useState('')
     const [files, setFiles] = useState('')
     const [redirect, setRedirect] = useState(false)
-    
-useEffect(()=>{
-    fetch('http://localhost:4000/post/' + id).then(response => {
-        response.json().then(postInfo =>{
-            setTitle(postInfo.title);
-            setContent(postInfo.content);
-            setSummary(postInfo.summary);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/post/' + id).then(response => {
+            response.json().then(postInfo => {
+                setTitle(postInfo.title);
+                setContent(postInfo.content);
+                setSummary(postInfo.summary);
+            })
         })
-    })
-},[])
+    }, [])
 
 
- 
-    const updatePost =async (e)=> {
+
+    const updatePost = async (e) => {
         e.preventDefault();
         const data = new FormData();
         data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    if(files?.[0]){
-        data.set('file', files?.[0])
-    }
- 
-       await fetch('http://localhost:4000/post',{
-            method: 'POST',
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('id',id);
+        if (files?.[0]) {
+            data.set('file', files?.[0])
+        }
+
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'PUT',
             body: data,
-        })
-        setRedirect(true);
+            credentials:'include',
+        });
+        if (response.ok) {
+            setRedirect(true);
+        }
+
     }
     if (redirect) {
-        return <Navigate to={'/post/'+id} />
+        return <Navigate to={'/post/' + id} />
     }
     return (
         <form onSubmit={updatePost}>
