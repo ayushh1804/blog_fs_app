763d887820dffda2c1fbe59b06f1165ec98848f5
AYUSH <ayushrajbls@gmail.com>
2023-04-23 21:56:04 +0530

    edit blog option added

--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -1,10 +1,13 @@
 import { formatISO9075 } from 'date-fns';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { UserContext } from '../UserContext';
+import { Link } from 'react-router-dom';
 
 const PostPage = () => {
     const { id } = useParams();
     const [postInfo, setPostInfo] = useState(null)
+    const { userInfo } = useContext(UserContext)
     useEffect(() => {
 
         fetch(`http://localhost:4000/post/${id}`).then(response => {
@@ -21,6 +24,11 @@ const PostPage = () => {
             <h1>{postInfo.title}</h1>
             <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
             <div className="author">by @{postInfo.author.username}</div>
+            {userInfo.id === postInfo.author._id && (
+                <div className='edit-row'> 
+                    <Link to={`/edit/${postInfo._id}`} className='edit-btn'>Edit this post</Link>
+                    </div>
+            )}
             <div className='image'>
                 <img src={`http://localhost:4000/${postInfo.cover}`} alt='' />
             </div>
